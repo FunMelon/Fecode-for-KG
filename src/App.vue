b11498d34da2e9d0ac7e04ea08b3afa45d782d86
FunMelon <Fun_Melon@163.com>
2024-03-20 11:13:54 +0800

    完成了整体大框架的布局

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    
-  </div>
+  <!-- 整个网页 -->
+  <a-flex class="whole" >
+    <!-- 左侧 -->
+    <a-flex class="left block" :style="{ flex: 1 }">
+      侧边
+    </a-flex>
+    <!-- 右侧 -->
+    <a-flex class="right block" vertical :style="{ flex: 5 }">
+      <!-- 顶部 -->
+      <a-flex class="top block" :style="{ flex: 1 }">
+        顶部
+      </a-flex>
+      <!-- 中部 -->
+      <a-flex class="middle block" :style="{ flex: 5 }">
+        中部
+      </a-flex>
+      <!-- 下部 -->
+      <a-flex class="down block" :style="{ flex: 3 }">
+        下部
+      </a-flex>
+    </a-flex>
+  </a-flex>
 </template>
 
 <script>
@@ -9,4 +28,41 @@
 </script>
 
 <style>
+/* 砖块，为了更加美观 */
+.block {
+  padding: 3px;
+  border: 1px solid #ccc;
+}
+
+/* 整个网页，设置这个属性为了占满空间 */
+.whole {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+
+/* 侧边栏 */
+.left {
+
+}
+
+/* 主要内容栏 */
+.right {
+
+}
+
+/* 顶部 */
+.top {
+
+}
+
+.middle {
+
+}
+
+.down {
+
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Antd).mount('#app');
